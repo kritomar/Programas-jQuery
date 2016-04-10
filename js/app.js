6f134d3cee061069ae05616bfb5ea46ade6b6e52
DOCENTE <DOCENTE>
2016-04-10 17:10:18 -0500

    modificacion de boton y spoiler

--- js/app.js
+++ js/app.js
@@ -3,10 +3,26 @@
 //mostrar el mensaje lentamente usamos atajo $
 //$(".advertencia").show("fast");
 
+
+// 1. Esconder spoiler
 $(".spoiler span").hide();
 
+
+// 2. Agrega boton
 $(".spoiler").append("<button> Mostrar Spoiler </button>");
 
+
+//3. Cuando se presiona el boton
+// Agregar un handler al evento click
+$(".spoiler button").click(function()
+	{
+
+	//3.1 Mostrar spoiler
+	$(".spoiler span").show();
+
+	//3.2 Deshacerse del boton
+	$(this).remove();
+	})
 /*
 function enviarAdvertencia()
 {
