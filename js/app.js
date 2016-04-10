8a723f4c1d6918980889da1d0bbca25ca96652b5
DOCENTE <DOCENTE>
2016-04-10 17:01:12 -0500

    jQuery imagen

---
+++ js/app.js
@@ -0,0 +1,17 @@
+//Esconder el mensaje
+//jQuery(".advertencia").hide();
+//mostrar el mensaje lentamente usamos atajo $
+//$(".advertencia").show("fast");
+
+$(".spoiler span").hide();
+
+$(".spoiler").append("<button> Mostrar Spoiler </button>");
+
+/*
+function enviarAdvertencia()
+{
+	jQuery(".advertencia").hide().show("slow");
+}
+//le pasamos al handler la funcion sin parentesis 
+$(document).ready(enviarAdvertencia);
+*/
